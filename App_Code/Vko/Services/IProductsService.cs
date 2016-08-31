087af9845a85d8dd8b27ad13f3eb49bf8ee75be6
windowsazure <windowsazure>
2016-08-31 22:27:01 +0000

    Changed order of methods

--- App_Code/Vko/Services/IProductsService.cs
+++ App_Code/Vko/Services/IProductsService.cs
@@ -12,45 +12,31 @@ namespace Vko.Services
 	public interface IProductsService
 	{
 		PagedResult<Cart> ListCarts(int from=0, int count=0);
-		
-		IEnumerable<Product> ListProducts(int from=0, int count=10);
-
-		IEnumerable<Supplier> ListSuppliers(int from=0, int count=10);
-		
-		PagedResult<Product> FindProductsPaged(string search);
-		
-		IEnumerable<Product> FindProducts<T>(T args);
-
-		IEnumerable<Supplier> FindSuppliers<T>(T args);
-		
-		IEnumerable<Category> FindCategories<T>(T args);
-		
-		PagedResult<Product> ListProductsPaged(int from=0, int count=10);
-		
-		Product CreateProduct(Product product);
-		
-		Product UpdateProduct(Product product);
-		
-		Supplier CreateSupplier(Supplier supplier);
-		
-		Supplier UpdateSupplier(Supplier supplier);
-					
-		Category CreateCategory(Category category);
-		
-		Category UpdateCategory(Category category);
-
-		IEnumerable<OrderDetail> ListOrderDetails(int from=0, int count=10);
-		
-		IEnumerable<Order> ListOrders(int from, int count);
-		
 		Cart AddToCart(int productId, decimal price);
-		
 		Cart RemoveFromCart(int productId);
-		
 		Cart CreateCart(DateTime cartDate);
 		
+		IEnumerable<Product> ListProducts(int from=0, int count=10);
+		PagedResult<Product> ListProductsPaged(int from=0, int count=10);
+		PagedResult<Product> FindProductsPaged(string search);
+		IEnumerable<Product> FindProducts<T>(T args);
+		Product CreateProduct(Product product);
+		Product UpdateProduct(Product product);
+
+		IEnumerable<Supplier> ListSuppliers(int from=0, int count=10);
+		IEnumerable<Supplier> FindSuppliers<T>(T args);
+		Supplier CreateSupplier(Supplier supplier);
+		Supplier UpdateSupplier(Supplier supplier);
+
 		IEnumerable<Category> ListCategories(int from=0, int count=10);
-		
+		IEnumerable<Category> FindCategories<T>(T args);
+		Category CreateCategory(Category category);
+		Category UpdateCategory(Category category);
+
+		IEnumerable<Order> ListOrders(int from, int count);
+
+		IEnumerable<OrderDetail> ListOrderDetails(int from=0, int count=10);
+
 		IEnumerable<object> Report();
 	}
 }
